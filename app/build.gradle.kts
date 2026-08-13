5fb046f0726423a7428d4e93f7d65854a6d0e90d
zixt <zixt233@gmail.com>
2026-08-14 02:10:54 +0800

    chore: release v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.zixt233.pirt"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
